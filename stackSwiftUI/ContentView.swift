361d46f71d08303de0654631b010964becc198d0
carlos erices <front.end.nicolas@gmail.com>
2020-08-03 12:04:07 -0400

    finish basic course VStack, HStack, ZStack width position inside simple layout

--- stackSwiftUI/ContentView.swift
+++ stackSwiftUI/ContentView.swift
@@ -13,31 +13,10 @@ struct ContentView: View {
         //Vertical stack for text title
         VStack {
             ExtractedView()
-            HStack {
-                
-                //box green
-                PricingView()
-                
-                //box gray
-                /*VStack{
-                    Text("Carrera")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                    
-                    Text("$10.99")
-                        .font(.system(size: 30, weight: .heavy, design: .rounded))
-                        .foregroundColor(Color.white)
-                    
-                    Text("Toda una carrera")
-                        .font(.system(size: 20, weight: .regular, design: .rounded))
-                        .foregroundColor(Color.white)
-                }
-                .frame(minWidth:0, maxWidth: .infinity, minHeight: 100)
-                .padding(30)
-                .background(Color.gray)
-                .cornerRadius(6)*/
-            }.padding(.horizontal)
+            HeadCard()
+            BigCard()
+            Spacer()
+            
         }
     }
 }
@@ -50,41 +29,86 @@ struct ContentView_Previews: PreviewProvider {
 
 struct ExtractedView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            Text("select your ")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
-            
-            
-            Text("itinerary for learning")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
-        }
+        HStack{
+            VStack(alignment: .leading, spacing: 4){
+                Text("Elige tu itinerario ")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                
+                
+                Text("de aprendeizaje")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            Spacer()
+        }.padding(15)
     }
 }
 
 struct PricingView: View {
     
- 
+    var title: String
+    var price: String
+    var icon: String?
+    var textColor: Color
+    var backgrounColor: Color
+    var subTitle: String
     
     var body: some View {
         VStack{
-            Text("Básico")
+            icon.map({
+                Image(systemName: $0)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            })
+            Text(title)
                 .font(.system(.title, design: .rounded))
                 .fontWeight(.bold)
-                .foregroundColor(.white)
+                .foregroundColor(textColor)
             
-            Text("$9.99")
+            Text(price)
                 .font(.system(size: 30, weight: .heavy, design: .rounded))
-                .foregroundColor(Color.white)
+                .foregroundColor(textColor)
             
-            Text("Un Curso Icluido")
+            Text(subTitle)
                 .font(.system(size: 20, weight: .regular, design: .rounded))
-                .foregroundColor(Color.white)
+                .foregroundColor(textColor)
         }
         .frame(minWidth:0, maxWidth: .infinity, minHeight: 100)
         .padding(30)
-        .background(Color.gray)
+        .background(backgrounColor)
         .cornerRadius(6)
+    }
+}
+
+struct HeadCard: View {
+    var body: some View {
+        HStack(spacing: 15) {
+            
+            //box green
+            PricingView(title: "Básico", price: "$9.99", textColor: Color.white, backgrounColor: Color.green, subTitle: "Un Curso Incluido")
+            ZStack{
+                //box blue
+                PricingView(title: "Carrera", price: "$9.99", textColor: Color.white, backgrounColor: Color.blue, subTitle: "Un carrera Incluida")
+                
+                Text("lo mejor para empezar")
+                    .font(.system(.caption, design: .rounded))
+                    .foregroundColor(Color.white)
+                    .padding(8)
+                    .cornerRadius(6)
+                    .background(Color.red)
+                    .offset(x: 0, y : -80)
+            }
+        }.padding(.horizontal)
+    }
+}
+
+struct BigCard: View {
+    var body: some View {
+        ZStack{
+            //box green
+            PricingView(title: "Definitivo", price: "$99.99", icon: "lightbulb", textColor: Color.white, backgrounColor: Color.yellow, subTitle: "Todo los cursos online")
+                .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+        }.padding(.vertical)
     }
 }
